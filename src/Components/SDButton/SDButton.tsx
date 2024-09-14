80aae44c896b7115439ec57f6f342385339b7ffe
enriquebds <enrique.barbosasilva@gmail.com>
2024-09-14 20:31:45 -0300

    feat: addind props on button

--- src/Components/SDButton/SDButton.tsx
+++ src/Components/SDButton/SDButton.tsx
@@ -1,3 +1,5 @@
-export const SDButton = () => {
-  return <button></button>;
+import { SDButtonProps } from "./SDButton.type";
+
+export const SDButton = ({ text, ...props }: SDButtonProps) => {
+  return <button {...props}>{text}</button>;
 };
